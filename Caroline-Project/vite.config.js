210aad7fcf8d2e92ef3c3ed087509e3e8edae243
LebohangDev <phxsoul39@gmail.com>
2026-01-02 19:16:24 +0400

    base change

--- Caroline-Project/vite.config.js
+++ Caroline-Project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Caroline-Project/',
+  base: '/Caroline/',
   plugins: [react()],
   server: {
 
